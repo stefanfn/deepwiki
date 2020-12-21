54a3ed8553de01be3d70f4844d989ef350baecd4
Stefan Sassenberg <stefan.sassenberg@gmx.de>
2020-12-21 20:16:25 +0100

    startet apache neu, wenn er beendet wird

--- files/start.sh
+++ files/start.sh
@@ -5,4 +5,10 @@ cd '/usr' && /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
 while (! mysql_upgrade -uroot); do sleep 1; done
 
 # Apache im Vordergrund starten
-apachectl -DFOREGROUND
+while (true); do
+  echo "starting apachingctl"
+  apachectl -DFOREGROUND
+  echo "apachectl ends, restarting in 2 seconds"
+  sleep 2
+done
+
